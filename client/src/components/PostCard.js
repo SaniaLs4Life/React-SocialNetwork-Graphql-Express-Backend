de871f8324f9e956dbe38b2a142df8a194c01a4c
Hakan Genc <gnchakan1@gmail.com>
2019-07-30 00:01:02 +0200

    Post Cards & Skeleton added

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Card, Image, Label, Icon } from "semantic-ui-react";
+import moment from "moment";
+import { Link } from "react-router-dom";
+
+function PostCard({
+  post: { body, createdAt, id, username, likeCount, commentCount, likes }
+}) {
+
+  const likePost = () => {
+      console.log('Post liked!');
+  }
+  
+  const commentOnPost = () => {
+      console.log('Commented on a post!');
+  }
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://www.pinclipart.com/picdir/middle/355-3553881_stockvader-predicted-adig-user-profile-icon-png-clipart.png"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Label style={{ cursor: "pointer" }} onClick={likePost}>
+          <Icon color="red" name="heart outline" /> {likeCount}
+        </Label>
+        <Label style={{ cursor: "pointer" }} onClick={commentOnPost}>
+          <Icon color="blue" name="comments outline" /> <b>{commentCount}</b>
+        </Label>
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default PostCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid, Header } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
+import PostSkeleton from "../components/PostSkeleton";
 
 function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  const {
+    loading,
+    data: { getPosts: posts }
+  } = useQuery(FETCH_POSTS_QUERY);
+  if (posts) {
+    console.log(posts);
+  }
+  return (
+    <Grid columns={3} divided>
+      <Grid.Row className="page-title">
+        <Header as="h3" textAlign="center" content="Recent Posts" />
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <PostSkeleton />
+        ) : (
+          posts &&
+          posts.map((post, i) => (
+            <Grid.Column key={i} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
+  );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      likeCount
+      username
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        body
+        createdAt
+      }
+    }
+  }
+`;
 
 export default Home;
